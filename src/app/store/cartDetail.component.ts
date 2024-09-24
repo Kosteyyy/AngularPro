8bd21ce8ae6b9c9d7cd144edb9fe6ed59a959817
Kosteyyy <7456052@gmail.com>
2024-09-24 20:45:16 +0300

    Change Readme.md

--- src/app/store/cartDetail.component.ts
+++ src/app/store/cartDetail.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { Cart } from '../model/cart.model';
 
 @Component({
-    template: `<div>
-        <h3 class="bg-info p-1 text-white">Cart Detail Component</h3>
-    </div>`,
+    selector: 'app-cart-detail',
+    templateUrl: 'cartDetail.component.html',
 })
-export class CartDetailComponent {}
+export class CartDetailComponent {
+    constructor (public cart: Cart) {
+
+    }
+}
