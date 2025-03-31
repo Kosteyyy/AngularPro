dc3cf767e9621332a9fb595386bee81595e96b2c
Kosteyyy <averkiev@yandex.ru>
2025-03-31 22:28:12 +0300

    Наполнение страниц админки
    Глава 7

--- src/app/admin/productEditor.component.ts
+++ src/app/admin/productEditor.component.ts
@@ -1,6 +1,32 @@
 import { Component } from '@angular/core';
+import { Product } from '../model/product.model';
+import { ProductRepository } from '../model/product.repository';
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
     selector: 'app-product-editor',
-    template: `<h3 style="padding-top: 10px">Product Editor Placeholder</h3>`,
+    templateUrl: 'productEditor.component.html',
 })
-export class ProductEditorComponent {}
+export class ProductEditorComponent {
+    editing: boolean = false;
+    product: Product = new Product();
+
+    constructor(
+        private repository: ProductRepository,
+        private router: Router,
+        activeRoute: ActivatedRoute
+    ) {
+        this.editing = activeRoute.snapshot.params['mode'] == 'edit';
+        console.log("🚀 ~ ProductEditorComponent ~ activeRoute.snapshot:", activeRoute.snapshot);
+        if (this.editing) {
+            Object.assign(
+                this.product,
+                repository.getProduct(activeRoute.snapshot.params['id'])
+            );
+        }
+    }
+
+    save() {
+        this.repository.saveProduct(this.product);
+        this.router.navigateByUrl('/admin/main/products');
+    }
+}
